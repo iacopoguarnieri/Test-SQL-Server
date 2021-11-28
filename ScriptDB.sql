fd0c4ee1059946f6cb73a516bc2fa6efe91db3f4
Iacopo Guarnieri <iacopo.gguarnieri@gmail.com>
2021-11-28 13:44:07 +0100

    Update ScriptDB.sql

--- ScriptDB.sql
+++ ScriptDB.sql
@@ -1,95 +1,68 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red15\green112\blue1;\red255\green255\blue254;\red25\green25\blue25;
-\red0\green0\blue255;\red19\green120\blue72;\red144\green1\blue18;}
-{\*\expandedcolortbl;;\cssrgb\c0\c50196\c0;\cssrgb\c100000\c100000\c99608;\cssrgb\c12941\c12941\c12941;
-\cssrgb\c0\c0\c100000;\cssrgb\c3529\c53333\c35294;\cssrgb\c63922\c8235\c8235;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl360\partightenfactor0
+/* Creo il database */
+CREATE DATABASE TestDB;
+ GO 
 
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 /* Creo il database */\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 DATABASE\cf4 \strokec4  TestDB;\cb1 \
-\cf5 \cb3 \strokec5 GO\cf4 \cb1 \strokec4 \
-\
-\cf5 \cb3 \strokec5 USE\cf4 \strokec4  TestDB;\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf2 \cb3 \strokec2 /* Creo le tabelle */\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 TABLE\cf4 \strokec4  ASSICURAZIONI(\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3     CodAss \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4  \cf5 \strokec5 PRIMARY\cf4 \strokec4  \cf5 \strokec5 KEY\cf4 \strokec4 ,\cb1 \
-\cb3     Nome \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     Sede \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \cb1 \strokec4 \
-\cb3 )\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 TABLE\cf4 \strokec4  PROPRIETARI(\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3     CodF \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4  \cf5 \strokec5 PRIMARY\cf4 \strokec4  \cf5 \strokec5 KEY\cf4 \strokec4 ,\cb1 \
-\cb3     Nome \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     Residenza \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \cb1 \strokec4 \
-\cb3 )\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 TABLE\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3     Targa \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4  \cf5 \strokec5 PRIMARY\cf4 \strokec4  \cf5 \strokec5 KEY\cf4 \strokec4 ,\cb1 \
-\cb3     Marca \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     Cilindrata \cf5 \strokec5 int\cf4 \strokec4  \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     Potenza \cf5 \strokec5 int\cf4 \strokec4  \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     CodF \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     CodAss \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     \cf5 \strokec5 FOREIGN KEY\cf4 \strokec4  (CodF) \cf5 \strokec5 REFERENCES\cf4 \strokec4  PROPRIETARI(CodF),\cb1 \
-\cb3     \cf5 \strokec5 FOREIGN KEY\cf4 \strokec4  (CodAss) \cf5 \strokec5 REFERENCES\cf4 \strokec4  ASSICURAZIONI(CodAss)\cb1 \
-\cb3 )\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 TABLE\cf4 \strokec4  SINISTRI(\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3     CodS \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4  \cf5 \strokec5 PRIMARY\cf4 \strokec4  \cf5 \strokec5 KEY\cf4 \strokec4 ,\cb1 \
-\cb3     Localita \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     \cf5 \strokec5 Data\cf4 \strokec4  \cf5 \strokec5 date\cf4 \strokec4  \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \cb1 \strokec4 \
-\cb3 )\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 CREATE\cf4 \strokec4  \cf5 \strokec5 TABLE\cf4 \strokec4  AUTOCOINVOLTE(\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3     CodS \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     Targa \cf5 \strokec5 varchar\cf4 \strokec4 (\cf6 \strokec6 255\cf4 \strokec4 ) \cf5 \strokec5 NOT\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \strokec4 ,\cb1 \
-\cb3     ImportoDelDanno \cf5 \strokec5 float\cf4 \strokec4  \cf5 \strokec5 NULL\cf4 \cb1 \strokec4 \
-\cb3     \cf5 \strokec5 PRIMARY\cf4 \strokec4  \cf5 \strokec5 KEY\cf4 \strokec4  (CodS,Targa)\cb1 \
-\cb3 )\cb1 \
-\
-\pard\pardeftab720\sl360\partightenfactor0
-\cf2 \cb3 \strokec2 /* Popolo le tabelle */\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  SINISTRI(CodS, Localita, \cf5 \strokec5 Data\cf4 \strokec4 ) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '1'\cf4 \strokec4 , \cf7 \strokec7 'Livorno'\cf4 \strokec4 , \cf7 \strokec7 '2021-10-19'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  SINISTRI(CodS, Localita, \cf5 \strokec5 Data\cf4 \strokec4 ) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '2'\cf4 \strokec4 , \cf7 \strokec7 'Pisa'\cf4 \strokec4 , \cf7 \strokec7 '2021-10-01'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  SINISTRI(CodS, Localita, \cf5 \strokec5 Data\cf4 \strokec4 ) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '3'\cf4 \strokec4 , \cf7 \strokec7 'Firenze'\cf4 \strokec4 , \cf7 \strokec7 '2021-10-15'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  SINISTRI(CodS, Localita, \cf5 \strokec5 Data\cf4 \strokec4 ) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '4'\cf4 \strokec4 , \cf7 \strokec7 'Roma'\cf4 \strokec4 , \cf7 \strokec7 '2021-10-19'\cf4 \strokec4 );\cb1 \
-\
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  PROPRIETARI(CodF, Nome, Residenza) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'ABCDEF123456'\cf4 \strokec4 , \cf7 \strokec7 'MARIO ROSSI'\cf4 \strokec4 , \cf7 \strokec7 'Livorno'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  PROPRIETARI(CodF, Nome, Residenza) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'ABFMGR23343'\cf4 \strokec4 , \cf7 \strokec7 'MARIO Bianchi'\cf4 \strokec4 , \cf7 \strokec7 'Pisa'\cf4 \strokec4 );\cb1 \
-\
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  ASSICURAZIONI(CodAss, Nome, Sede) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'ALZ'\cf4 \strokec4 , \cf7 \strokec7 'Allianz'\cf4 \strokec4 , \cf7 \strokec7 'Pisa'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  ASSICURAZIONI(CodAss, Nome, Sede) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'SR'\cf4 \strokec4 , \cf7 \strokec7 'Sara'\cf4 \strokec4 , \cf7 \strokec7 'Livorno'\cf4 \strokec4 );\cb1 \
-\
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'AB123CD'\cf4 \strokec4 , \cf7 \strokec7 'Nissan'\cf4 \strokec4 , \cf6 \strokec6 2001\cf4 \strokec4 , \cf6 \strokec6 121\cf4 \strokec4 , \cf7 \strokec7 'ABCDEF123456'\cf4 \strokec4 , \cf7 \strokec7 'ALZ'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'CD123AB'\cf4 \strokec4 , \cf7 \strokec7 'BMW'\cf4 \strokec4 , \cf6 \strokec6 300\cf4 \strokec4 , \cf6 \strokec6 121\cf4 \strokec4 , \cf7 \strokec7 'ABCDEF123456'\cf4 \strokec4 , \cf7 \strokec7 'SR'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'GN459GI'\cf4 \strokec4 , \cf7 \strokec7 'Fiat'\cf4 \strokec4 , \cf6 \strokec6 300\cf4 \strokec4 , \cf6 \strokec6 400\cf4 \strokec4 , \cf7 \strokec7 'ABFMGR23343'\cf4 \strokec4 , \cf7 \strokec7 'SR'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'GO037DU'\cf4 \strokec4 , \cf7 \strokec7 'Fiat'\cf4 \strokec4 , \cf6 \strokec6 300\cf4 \strokec4 , \cf6 \strokec6 400\cf4 \strokec4 , \cf7 \strokec7 'ABFMGR23343'\cf4 \strokec4 , \cf7 \strokec7 'ALZ'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  \cf5 \strokec5 AUTO\cf4 \strokec4 (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 'TI890RI'\cf4 \strokec4 , \cf7 \strokec7 'Fiat'\cf4 \strokec4 , \cf6 \strokec6 2000\cf4 \strokec4 , \cf6 \strokec6 400\cf4 \strokec4 , \cf7 \strokec7 'ABFMGR23343'\cf4 \strokec4 , \cf7 \strokec7 'SR'\cf4 \strokec4 );\cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf4 \cb3   \cb1 \
-\pard\pardeftab720\sl360\partightenfactor0
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '1'\cf4 \strokec4 , \cf7 \strokec7 'AB123CD'\cf4 \strokec4 , \cf7 \strokec7 '400'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '1'\cf4 \strokec4 , \cf7 \strokec7 'TI890RI'\cf4 \strokec4 , \cf7 \strokec7 '300'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '2'\cf4 \strokec4 , \cf7 \strokec7 'GN459GI'\cf4 \strokec4 , \cf7 \strokec7 '300'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '2'\cf4 \strokec4 , \cf7 \strokec7 'TI890RI'\cf4 \strokec4 , \cf7 \strokec7 '300'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '6'\cf4 \strokec4 , \cf7 \strokec7 'TI890RI'\cf4 \strokec4 , \cf7 \strokec7 '300'\cf4 \strokec4 );\cb1 \
-\cf5 \cb3 \strokec5 INSERT\cf4 \strokec4  \cf5 \strokec5 INTO\cf4 \strokec4  AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) \cf5 \strokec5 VALUES\cf4 \strokec4 (\cf7 \strokec7 '7'\cf4 \strokec4 , \cf7 \strokec7 'GO037DU'\cf4 \strokec4 , \cf7 \strokec7 '300'\cf4 \strokec4 );\cb1 \
+ USE TestDB; 
+
+ /* Creo le tabelle */ 
+ CREATE TABLE ASSICURAZIONI( 
+  CodAss varchar  (  255  ) NOT NULL PRIMARY KEY  , 
+ Nome varchar  (  255  ) NOT NULL  , 
+ Sede varchar  (  255  ) NOT NULL 
+ ) 
+
+ CREATE TABLE PROPRIETARI( 
+  CodF varchar  (  255  ) NOT NULL PRIMARY KEY  , 
+ Nome varchar  (  255  ) NOT NULL  , 
+ Residenza varchar  (  255  ) NOT NULL 
+ ) 
+
+ CREATE TABLE AUTO  ( 
+  Targa varchar  (  255  ) NOT NULL PRIMARY KEY  , 
+ Marca varchar  (  255  ) NOT NULL  , 
+ Cilindrata int NOT NULL  , 
+ Potenza int NOT NULL  , 
+ CodF varchar  (  255  ) NOT NULL  , 
+ CodAss varchar  (  255  ) NOT NULL  , 
+  FOREIGN KEY (CodF) REFERENCES PROPRIETARI(CodF), 
+  FOREIGN KEY (CodAss) REFERENCES ASSICURAZIONI(CodAss) 
+ ) 
+
+ CREATE TABLE SINISTRI( 
+  CodS varchar  (  255  ) NOT NULL PRIMARY KEY  , 
+ Localita varchar  (  255  ) NOT NULL  , 
+  Data date NOT NULL 
+ ) 
+
+ CREATE TABLE AUTOCOINVOLTE( 
+  CodS varchar  (  255  ) NOT NULL  , 
+ Targa varchar  (  255  ) NOT NULL  , 
+ ImportoDelDanno float NULL 
+  PRIMARY KEY (CodS,Targa) 
+ ) 
+
+ /* Popolo le tabelle */ 
+ INSERT INTO SINISTRI(CodS, Localita, Data  ) VALUES  (  '1'  , 'Livorno'  , '2021-10-19'  ); 
+ INSERT INTO SINISTRI(CodS, Localita, Data  ) VALUES  (  '2'  , 'Pisa'  , '2021-10-01'  ); 
+ INSERT INTO SINISTRI(CodS, Localita, Data  ) VALUES  (  '3'  , 'Firenze'  , '2021-10-15'  ); 
+ INSERT INTO SINISTRI(CodS, Localita, Data  ) VALUES  (  '4'  , 'Roma'  , '2021-10-19'  ); 
+
+ INSERT INTO PROPRIETARI(CodF, Nome, Residenza) VALUES  (  'ABCDEF123456'  , 'MARIO ROSSI'  , 'Livorno'  ); 
+ INSERT INTO PROPRIETARI(CodF, Nome, Residenza) VALUES  (  'ABFMGR23343'  , 'MARIO Bianchi'  , 'Pisa'  ); 
+
+ INSERT INTO ASSICURAZIONI(CodAss, Nome, Sede) VALUES  (  'ALZ'  , 'Allianz'  , 'Pisa'  ); 
+ INSERT INTO ASSICURAZIONI(CodAss, Nome, Sede) VALUES  (  'SR'  , 'Sara'  , 'Livorno'  ); 
+
+ INSERT INTO AUTO  (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) VALUES  (  'AB123CD'  , 'Nissan'  , 2001  , 121  , 'ABCDEF123456'  , 'ALZ'  ); 
+ INSERT INTO AUTO  (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) VALUES  (  'CD123AB'  , 'BMW'  , 300  , 121  , 'ABCDEF123456'  , 'SR'  ); 
+ INSERT INTO AUTO  (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) VALUES  (  'GN459GI'  , 'Fiat'  , 300  , 400  , 'ABFMGR23343'  , 'SR'  ); 
+ INSERT INTO AUTO  (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) VALUES  (  'GO037DU'  , 'Fiat'  , 300  , 400  , 'ABFMGR23343'  , 'ALZ'  ); 
+ INSERT INTO AUTO  (Targa, Marca, Cilindrata, Potenza, CodF, CodAss) VALUES  (  'TI890RI'  , 'Fiat'  , 2000  , 400  , 'ABFMGR23343'  , 'SR'  ); 
+ 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '1'  , 'AB123CD'  , '400'  ); 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '1'  , 'TI890RI'  , '300'  ); 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '2'  , 'GN459GI'  , '300'  ); 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '2'  , 'TI890RI'  , '300'  ); 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '6'  , 'TI890RI'  , '300'  ); 
+ INSERT INTO AUTOCOINVOLTE(CodS, Targa, ImportoDelDanno) VALUES  (  '7'  , 'GO037DU'  , '300'  ); 
 }
